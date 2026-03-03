0a4db793c77c11521800a525dbbed6aadec5950d
Lars Kruse <lars@lakruzz.com>
2026-03-03 13:46:35 +0000

    Setup TakT and workflows

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,23 @@ set -eou pipefail
 PREFIX="🍰  "
 echo "$PREFIX Running $(basename $0)"
 
+# GitHub CLI Dependencies
+set +e
+gh auth status >/dev/null 2>&1
+AUTH_OK=$?
+set -e
+if [ $AUTH_OK -ne 0 ]; then
+  echo "$PREFIX ⚠️ Not logged into GitHub CLI"
+  echo "$PREFIX    This is not looking good  — we want GitHub CLI to work!"
+else
+  echo "$PREFIX ✅ GitHub Authentication is working smooth!"
+  gh extension install devx-cafe/gh-tt
+  echo "$PREFIX ✅ Installed the TakT gh cli extension from devx-cafe/gh-tt "
+  gh alias import .devcontainer/.gh_alias.yml --clobber
+  echo "$PREFIX ✅ Installed the gh shorthand aliases"    
+  
+fi
+
 git config --global --add safe.directory $(pwd)
 echo "$PREFIX ✅ Setting up safe git repository to prevent dubious ownership errors"
 
